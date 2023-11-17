9175705ad029bf8958f935a71462ea4d28b33600
David Corvoysier <david@huggingface.co>
2023-11-17 16:59:44 +0100

    batch_size 4 is too much for llama 7b on 8xlarge

--- deploy_demo.sh
+++ deploy_demo.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 LLM_IMAGE="754289655784.dkr.ecr.us-east-2.amazonaws.com/neuronx-tgi:1.0.2-0.0.14dev"
-MODEL_ID="aws-neuron/Llama-2-7b-chat-hf-seqlen-2048-bs-4"
+MODEL_ID="aws-neuron/Llama-2-7b-chat-hf-seqlen-2048-bs-2"
 INSTANCE_TYPE="ml.inf2.8xlarge"
 REGION="us-east-2"
 
