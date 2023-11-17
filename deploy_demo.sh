88070dbbd288835b97db5f2a489a480d2c8adb69
David Corvoysier <david@huggingface.co>
2023-11-17 11:18:36 +0100

    deploy demo with llama model

--- deploy_demo.sh
+++ deploy_demo.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 LLM_IMAGE="754289655784.dkr.ecr.us-east-2.amazonaws.com/neuronx-tgi:1.0.2-0.0.14dev"
-MODEL_ID="aws-neuron/gpt2-seqlen-1024-bs-16"
-INSTANCE_TYPE="ml.inf2.xlarge"
+MODEL_ID="aws-neuron/Llama-2-7b-chat-hf-seqlen-2048-bs-4"
+INSTANCE_TYPE="ml.inf2.8xlarge"
 REGION="us-east-2"
 
 python deploy_neuronx_tgi.py \
