c226f31a39b2c601517fed06c65edea3fb66ff1e
David Corvoysier <david@huggingface.co>
2025-03-11 10:51:04 +0100

    feat: update deploy snippet

--- deploy_snippet.py
+++ deploy_snippet.py
@@ -1,24 +1,28 @@
 import boto3
 import os
-from sagemaker.huggingface import HuggingFaceModel, get_huggingface_llm_image_uri
+from sagemaker.huggingface import HuggingFaceModel
 
+#role = get_execution_role()
 iam = boto3.client("iam")
 role = iam.get_role(RoleName="sagemaker_execution_role")["Role"]["Arn"]
 
 # Hub Model configuration. https://huggingface.co/models
 hub = {
-    "HF_MODEL_ID": "meta-llama/Meta-Llama-3-8B",
+    "HF_MODEL_ID": "meta-llama/Llama-3.2-1B-Instruct",
     "HF_NUM_CORES": "2",
-    "HF_AUTO_CAST_TYPE": "fp16",
-    "MAX_BATCH_SIZE": "8",
-    "MAX_INPUT_LENGTH": "3686",
+    "HF_AUTO_CAST_TYPE": "bf16",
+    "MAX_BATCH_SIZE": "1",
+    "MAX_INPUT_TOKENS": "3686",
     "MAX_TOTAL_TOKENS": "4096",
     "HF_TOKEN": os.environ["HF_TOKEN"],
 }
 
+region = boto3.Session().region_name
+image_uri = f"763104351884.dkr.ecr.{region}.amazonaws.com/huggingface-pytorch-tgi-inference:2.1.2-optimum0.0.27-neuronx-py310-ubuntu22.04"
+
 # create Hugging Face Model Class
 huggingface_model = HuggingFaceModel(
-    image_uri=get_huggingface_llm_image_uri("huggingface-neuronx", version="0.0.24"),
+    image_uri=image_uri,
     env=hub,
     role=role,
 )
@@ -32,15 +36,15 @@ predictor = huggingface_model.deploy(
 )
 
 # send request
-predictor.predict(
+output = predictor.predict(
     {
-        "inputs": "What is is the capital of France?",
+        "inputs": "Write a function to generate random numbers in python",
         "parameters": {
             "do_sample": True,
-            "max_new_tokens": 128,
-            "temperature": 0.7,
-            "top_k": 50,
-            "top_p": 0.95,
+            "max_new_tokens": 256,
+            "temperature": 0.1,
+            "top_k": 10,
         }
     }
 )
+print(output)
