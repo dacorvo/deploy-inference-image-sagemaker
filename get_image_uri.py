8262cb6ad2f86325f34e0bf3ee613b7bf6824fd2
David Corvoysier <david@huggingface.co>
2026-01-15 15:10:42 +0000

    feat: add script to get the image uri

---
+++ get_image_uri.py
@@ -0,0 +1,43 @@
+import argparse
+import boto3
+from sagemaker.core import image_uris
+
+
+def retrieve_image_uri(region: str,
+                       framework: str | None = "huggingface-vllm-neuronx",
+                       version: str | None = None) -> str:
+    """
+    Retrieve the image URI for a given framework and model.
+
+    Args:
+        framework (str): The framework name (e.g., 'huggingface-vllm-neuronx').
+        region (str): The AWS region.
+        version (str | None): The version of the model.
+
+    Returns:
+        str: The image URI.
+    """
+    image_uri = image_uris.retrieve(
+        framework=framework,
+        region=region,
+        version=version
+    )
+    return image_uri
+
+
+if __name__ == "__main__":
+    session = boto3.session.Session()
+    current_region = session.region_name
+
+    parser = argparse.ArgumentParser(description="Retrieve the Sagemaker image URI for a given framework and model")
+    parser.add_argument("--framework", type=str, default="huggingface-vllm-neuronx", help="The framework name")
+    parser.add_argument("--region", type=str, default="us-east-1" if current_region is None else current_region, help="The AWS region")
+    parser.add_argument("--version", type=str, default=None, help="The version of the model")
+    args = parser.parse_args()
+
+    uri = retrieve_image_uri(
+        framework=args.framework,
+        region=args.region,
+        version=args.version
+    )
+    print(f"Image URI: {uri}")
